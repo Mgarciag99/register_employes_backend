412a3f4edb217a7d24f0562e91af099eedbf3618
Mgarciag99 <marcopc303@gmail.com>
2024-09-10 15:43:52 -0600

    feat: modify endpoints get list

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,18 +8,26 @@ import { DepartmentModule } from './department/department.module';
 import { MunicipalityModule } from './municipality/municipality.module';
 import { CompanyModule } from './company/company.module';
 import { EmployeModule } from './employe/employe.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: '',
-      database: 'register_employers',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        type: configService.get<string>('DATABASE_TYPE') as 'mysql',
+        host: configService.get<string>('DATABASE_HOST'),
+        port: +configService.get<number>('DATABASE_PORT'),
+        username: configService.get<string>('DATABASE_USERNAME'),
+        password: configService.get<string>('DATABASE_PASSWORD'),
+        database: configService.get<string>('DATABASE_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true, // Set to false in production
+      }),
     }),
     UsersModule,
     CountriesModule,
